144cdcb069853313a4eb93c9328f3cae9bb2d21b
PamanAleph <alieffirmandabisnis@gmail.com>
2024-06-24 15:11:05 +0700

    chore: image dotted

--- src/components/cabinet/Header.tsx
+++ src/components/cabinet/Header.tsx
@@ -1,7 +1,6 @@
-import Image from 'next/image';
-import React from 'react'
-import VECTOR1 from "@/assets/icon/Vector 1.png"
-import VECTOR2 from "@/assets/icon/Vector 2.png"
+import Image from "next/image";
+import React from "react";
+import DOTTED from "@/assets/background/Path.png"
 
 interface HeaderProps {
     title: string;
@@ -12,14 +11,14 @@ interface HeaderProps {
 export default function Header({ title, description, image }: HeaderProps) {
     return (
         <section>
-            <div className="flex flex-col space-y-12 px-4 py-[2rem] md:px-[10rem] bg-[#E5E7EB]">
+            <div className="flex flex-col space-y-12 bg-[#E5E7EB] px-4 py-[2rem] md:px-[10rem]">
                 {/* text logo nya  */}
                 <div className="flex flex-col border-l-4 border-[#1FA820] pl-4 md:pl-8">
                     <h1 className="text-[2rem] font-[900] uppercase tracking-widest md:text-[3rem]">
                         {title}
                     </h1>
                     <div className="flex flex-col items-start gap-2 md:flex-row md:items-center">
-                        <h1 className="text-[2rem] font-thin uppercase tracking-widest text-stroke-1 text-stroke-black text-stroke-fill-white md:text-[3rem]">
+                        <h1 className="font-thin text-[2rem] uppercase tracking-widest text-stroke-1 text-stroke-black text-stroke-fill-white md:text-[3rem]">
                             Division
                         </h1>
                         <h1 className="text-justify text-[0.8rem] md:max-w-[20rem]">
@@ -27,10 +26,27 @@ export default function Header({ title, description, image }: HeaderProps) {
                         </h1>
                     </div>
                 </div>
-
                 {/* gambarnya  */}
-                <Image src={image} alt={`${title}'s Image`} width={1080} height={720} className='h-[200px] w-full rounded-lg md:h-[363px] md:w-[645px]' />
+                <div className="relative py-5">
+                    <Image
+                        src={image}
+                        alt={`${title}'s Image`}
+                        width={1080}
+                        height={720}
+                        className="relative h-[200px] w-full rounded-lg md:h-[363px] md:w-[645px] z-30"
+                    />
+
+                    <Image
+                        src={DOTTED}
+                        alt="Dotted"
+                        height={1080}
+                        width={1920}
+                        className="hidden md:block absolute top-[67px] left-[325px] z-10 h-[350px] w-[350px]"
+                    />
+                </div>
+
+
             </div>
         </section>
-    )
+    );
 }
